89104bcc73910e5475c09851fe2a026aaee55a15
Andrey Antukh <niwi@niwi.nz>
2022-07-18 16:03:48 +0200

    :fire: Remove incorrect license term from many files

--- src/stks/util/pprint.cljs
+++ src/stks/util/pprint.cljs
@@ -2,7 +2,7 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ;;
-;; Copyright (c) UXBOX Labs SL
+;; Copyright (c) Andrey Antukh <niwi@niwi.nz>
 
 (ns stks.util.pprint
   (:refer-clojure :exclude [prn])
